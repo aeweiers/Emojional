8c07c5503dc349a16f0f52ea93d9ae7a088d788e
Abbey Weiers <aeweiers@gmai.com>
2019-06-15 12:48:51 -0500

    Initial Commit

---
+++ Emojional Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional Project
+//
+//  Created by Abigail Weiers on 6/15/19.
+//  Copyright © 2019 Abigail Weiers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
